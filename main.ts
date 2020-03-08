b63a7646d9132ffc59d25563b4b6fbb6760decd3
techtigers33958 <53841748+techtigers33958@users.noreply.github.com>
2020-03-08 10:03:46 -0400

    Commented and Finished Receive-Transmit

--- main.ts
+++ main.ts
@@ -1,24 +1,57 @@
-function releaseBall () {
-    radio.setGroup(50)
-    pins.servoWritePin(AnalogPin.P0, 45)
-    basic.pause(1000)
-    pins.servoWritePin(AnalogPin.P0, 180)
+/**
+ * Clears the screen
+ */
+/**
+ * This function releases the ball when called
+ */
+/**
+ * On start:
+ * 
+ * - Set the operations list to the operations you need
+ * 
+ * - Select the operation and show it
+ * 
+ * -Pick a random number and send it
+ */
+/**
+ * This function contains an array which picks a random operation
+ */
+/**
+ * This function will calculate the answer based on the random number and the operation
+ */
+function reset () {
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 }
+/**
+ * WHen you receive the string:
+ * 
+ * - If the expected (calculated answer me have) = to the number you send, you succeed
+ * 
+ * -If not, you lose and we reset
+ */
 radio.onReceivedString(function (receivedString) {
     let recievedNumber = 0
     if (expectedAnswer == recievedNumber) {
         basic.showIcon(IconNames.Yes)
         releaseBall()
     } else {
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
+        basic.showIcon(IconNames.No)
+        reset()
     }
 })
+function releaseBall () {
+    radio.setGroup(50)
+    pins.servoWritePin(AnalogPin.P0, 45)
+    basic.pause(1000)
+    pins.servoWritePin(AnalogPin.P0, 180)
+}
 function calculateAnswer (operation: string, operand: number) {
     if (operation == "DBL") {
         expectedAnswer = operand * 2
@@ -28,8 +61,6 @@ function calculateAnswer (operation: string, operand: number) {
         expectedAnswer = operand / 2
     } else if (operation == "DEC") {
         expectedAnswer = operand - 1
-    } else {
-    	
     }
 }
 function selectOperation () {
@@ -42,6 +73,6 @@ let operation = ""
 let operationsList: string[] = []
 operationsList = ["DBL", "INC", "HLF", "DEC"]
 selectOperation()
-basic.showString("" + (operation))
+basic.showString(operation)
 let operand = Math.randomRange(0, 100)
 radio.sendValue(operation, operand)
